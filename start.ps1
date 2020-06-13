3189d0a4d3130237b0377020e8ffec2ff7fe442a
slanthie <slanthie@hotmail.com>
2020-06-13 12:31:52 -0400

    First version

---
+++ start.ps1
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Start the docker container which will keep looking for images inside
+# the inputs/ directory and spew out results into outputs/
+
+$PORT=80
+$pwd=(Get-Location)
+docker run --name deepdream-files -v $pwd\deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
+docker run --name deepdream-compute -v $pwd\deepdream:/opt/deepdream -d visionai/clouddream /bin/bash -c "cd /opt/deepdream && ./process_images.sh 2>&1 > log.html"
+docker run --name deepdream-json --volumes-from deepdream-compute -d ubuntu:14.04 /bin/bash -c "cd /opt/deepdream && ./make_json.sh"
+
---
+++ stop.ps1
@@ -0,0 +1,6 @@
+
+# Start the docker container which will keep looking for images inside
+# the inputs/ directory and spew out results into outputs/
+
+docker stop deepdream-files deepdream-compute deepdream-json deepdream-enter
+docker rm deepdream-files deepdream-compute deepdream-json deepdream-enter
