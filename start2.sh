c3249020b01f136e0da6374d25460d81704eb9fa
slanthie <slanthie@hotmail.com>
2020-06-14 13:38:46 -0400

    Updated files for windows. Use start2.sh in git bash on windows. Modify path accordingly. Do dos2unix on all files when downloading

---
+++ start2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Start the docker container which will keep looking for images inside
+# the inputs/ directory and spew out results into outputs/
+
+PORT=80
+docker run --name deepdream-files -v //c/Users/slant/clouddream/deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
+
+docker run --name deepdream-compute -v //c/Users/slant/clouddream/deepdream:/opt/deepdream -d   visionai/clouddream bash -c "cd /opt/deepdream && ./process_images.sh 2>&1 > log.html"
+
+docker run --name deepdream-json --volumes-from deepdream-compute -d ubuntu:14.04 bash -c "cd /opt/deepdream && ./make_json.sh"
